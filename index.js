cbedf8119a53aafefe7bccc600d1de935fb62c59
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-05-06 00:40:26 +0400

    changed autocomplete color

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-//test if our file works properly
-console.log('Hey, this works!')
-
 //handle message submission
 document.querySelector('form').addEventListener('submit', event => {
     //run a function every time the form is submitted
     event.preventDefault()
+
+    //hide existing form whenever a user creates a message
+    document.querySelector('#message-form').classList.add('hide')
+    document.querySelector('#link-form').classList.remove('hide')
 
     //get the message submitted
     const input = document.querySelector('#message-input')
@@ -16,3 +17,4 @@ document.querySelector('form').addEventListener('submit', event => {
     linkInput.value = `${window.location}#${encrypted}`
     linkInput.select()
 })
+
